49349a56067a13fe479ebd2e1be091ff8871055c
MayorMonty <artificalchamber@gmail.com>
2017-01-11 13:38:30 -0500

    Add team member and mentor support

---
+++ static/js/team.js
@@ -0,0 +1,29 @@
+var membersAnimate = function() {
+  var members = $('#fh5co-members');
+  if ( members.length > 0 ) {
+
+    members.waypoint( function( direction ) {
+
+      if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+
+        setTimeout(function() {
+          members.find('.to-animate').each(function( k ) {
+            var el = $(this);
+
+            setTimeout ( function () {
+              el.addClass('fadeInUp animated');
+            },  k * 200, 'easeInOutExpo' );
+
+          });
+        }, 200);
+
+
+
+        $(this.element).addClass('animated');
+
+      }
+    } , { offset: '80%' } );
+
+  }
+};
