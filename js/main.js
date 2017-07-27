a796be67fa4355a68fa703799d85a7a62f73ac63
Brendan McGuire <artificalchamber@gmail.com>
2017-07-27 10:41:05 -0400

    Scroll support

--- js/main.js
+++ js/main.js
@@ -8,6 +8,49 @@ window.addEventListener("load", function() {
         $("header ul.links").classList.toggle("active");
     });
 
-
+    var links = document.querySelectorAll("header ul.links li a");
+    for(var i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function(e) {
+            e.preventDefault();
+            scrollTo($(e.target["data-scrollTo"]));
+            for (var x = 0; x < links.length; x++) links[x].classList.remove("active");
+            e.target.classList.add("active");
+        });
+    }
 
 });
+
+/**
+ * Linearly Scrolls to a specified element on the page
+ * @param {Element} element 
+ * @param {Number} duration The duration of the scroll, in ms
+ * @param {Function} callback
+ */
+function scrollTo(element, duration, callback) {
+    if(!element) return;
+    var startingY = window.pageYOffset;
+    var diff = getYOffset(element) - startingY;
+    var start;
+
+    requestAnimationFrame(function go(timestamp) {
+        if(!start) start = timestamp;
+        var delta = timestamp - start, // Elapsed time
+            percent = Math.min(time / duration, 1); // [0, 1]
+
+        scrollTo(0, startingY + diff * percent);
+        if (time < duration) {
+            window.requestAnimationFrame(go);
+        } else {
+            callback && callback();
+        }
+    });
+}
+
+/**
+ * Returns the y offset of the specified element
+ * @param {Element} element 
+ */
+function getYOffset(element) {
+    var rec = document.getElementById(element).getBoundingClientRect();
+    return rec.top + window.scrollY;
+}
